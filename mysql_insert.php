f37576d7bbdbed75a351a010cd82bd6050e45b6c
Cody <iamcody.githyb@gmail.com>
2023-05-24 10:43:27 +0545

    php code push

--- mysql_insert.php
+++ mysql_insert.php
@@ -3,17 +3,62 @@ require_once"./mysql.php";
 
 $sql = "
        INSERT INTO students (name, rollno) VALUES ('cody', 01) ;
-       
-
+       ";
+       $sql2= "
+       INSERT INTO students (name, rollno) VALUES ('john', 02) ;
        ";
 
+       $sql3= "
+       INSERT INTO students (name, rollno) VALUES ('enoz', 03) ;
+       ";
+       $sql4= "
+       INSERT INTO students (name, rollno) VALUES ('messi', 04) ;
+    
+       ";
+       $sql5= "
+       INSERT INTO students (name, rollno) VALUES ('dimaria', 05) ;
+       ";
        
 $success = $connection -> query($sql);
 if ($success) {
-echo "record inserted successfully";
+echo "record inserted successfully <br>";
 
 }else{
-    echo "error inserting recording data";
+    echo "error inserting recording data <br>";
 }
+
+$success = $connection -> query($sql2);
+if ($success) {
+echo "record inserted successfully <br>";
+
+}else{
+    echo "error inserting recording data <br>";
+}
+
+$success = $connection -> query($sql3);
+if ($success) {
+echo "record inserted successfully <br>";
+
+}else{
+    echo "error inserting recording data <br>";
+}
+
+$success = $connection -> query($sql4);
+if ($success) {
+echo "record inserted successfully <br>";
+
+}else{
+    echo "error inserting recording data <br>";
+}
+
+$success = $connection -> query($sql5);
+if ($success) {
+echo "record inserted successfully <br>";
+
+}else{
+    echo "error inserting recording data <br>";
+}
+
+
 
 ?>
--- mysql_selectspecific.php
+++ mysql_selectspecific.php
@@ -2,4 +2,20 @@
 // homework
 // select student whose roll no is grater than 2
 
+require_once"./mysql.php";
+
+$sql = "select *from students
+        where rollno > 2
+";
+
+$result = $connection -> query($sql);
+if($result -> num_rows >0){
+    $data = []; 
+    while($row = $result -> fetch_assoc()){   
+     array_push($data, $row);
+    }
+}
+echo "<pre>";
+print_r($data);
+
 ?>
