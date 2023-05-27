2af755a9ce213cc1f5af15f3f4f4c29866d338ee
Cody <iamcody.githyb@gmail.com>
2023-05-27 14:43:17 +0545

    modified

---
+++ oop/class.php
@@ -0,0 +1,76 @@
+<?php
+
+class BankAccount {
+    protected $amount;
+
+    protected $interestRate;
+
+public  function __construct($openingBalance){
+    $this -> amount = $openingBalance;
+
+}
+
+     public function withdraw($withDrawAmount){
+        if($withDrawAmount > $this ->amount){
+            echo "insufficient balanace <br>";
+            return;
+        }
+
+        $this -> amount = $this -> amount - $withDrawAmount;
+    echo "withdraw account $withDrawAmount <br>";
+   }
+
+   public function deposit($depositAmount){
+    $this -> amount =$depositAmount;
+   }
+
+   public function checkBalance(){
+    echo "your balance amount is : " .$this -> amount ."<br>";
+   }
+
+   public function calculateInterest ($year){
+    echo "this interest :".($this -> amount * $year * $this -> interestRate)/100;
+}
+
+}
+
+
+class FixedDepositAccount extends BankAccount{
+
+    protected $interestRate = 12;
+public function withdraw($withDrawAmount)
+{
+    echo "total interest :"."cannot withdraw in fixed account<br>";
+    return;
+}   
+}
+
+class SavingAccount extends BankAccount{
+
+    protected $interestRate = 6;
+
+    public function withdraw($withDrawAmount)
+    {
+    if($withDrawAmount > 100000){
+        echo "canot withdraw more than 1 lakh <br>";
+        return;
+    }
+    parent::withdraw($withDrawAmount);
+}
+
+}
+
+// $JohnBankAccount = new BankAccount();
+// $JohnBankAccount -> withdraw(2000);
+
+// $BenBankAccount = new BankAccount(100000);
+// $BenBankAccount -> deposit(20000);
+// $BenBankAccount -> withdraw(30000);
+
+
+// $BenBankAccount = new FixedDepositAccount(100000);
+ $BenBankAccount =new FixedDepositAccount(100000);
+$BenBankAccount -> checkBalance();
+// $BenBankAccount -> withdraw(200000);
+$BenBankAccount ->calculateInterest(2);
+?>
